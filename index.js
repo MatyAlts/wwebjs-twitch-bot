0cea9c98957473f0c6bee0cd3c9e55ad5ebad6f7
Matias Torres <matiass.torres@alumnos.frm.utn.edu.ar>
2024-05-21 17:55:19 -0300

    v2

--- index.js
+++ index.js
@@ -147,16 +147,12 @@ client.on('message', async (msg) => {
 });
 
 client.on('message', async (msg) =>{
-    try{
         if (msg.body == '!soysub'){
             const chat = await msg.getChat();
             if(!chat.isGroup){
             msg.reply('Para verificar que seas subscriptor, sigue los pasos a continuacion:\n\nEnvia una captura de tu suscripcion junto al comando \n!verificar\n\nNuestros moderadores verificaran si eres suscriptor y te agregarán al grupo de whatsapp.')
         }
     } else msg.reply('Este comando solo esta disponible en privado.')
-    } catch (error){
-        console.log(error);
-    }
     
 })
 
