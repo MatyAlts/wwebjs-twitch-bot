c3f02bb819321d3b61d9e151376d71d6aa74d932
MatyAlts <matiass.torres@alumnos.frm.utn.edu.ar>
2024-09-05 11:57:51 -0300

    fixed auth not working

--- index.js
+++ index.js
@@ -46,16 +46,15 @@ const nombreArchivoDescargado = 'archivo_descargado.jpg';
 
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: "sessionss3",
-    }),
+    authStrategy: new LocalAuth(), // your authstrategy here
     puppeteer: {
-        // puppeteer args here
+      headless: true , args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
     webVersionCache: {
         type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-    }});
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2402.5-beta.html`,
+    },
+});
 
 // Save session values to the file upon successful auth
 client.on('authenticated', (session) => {
