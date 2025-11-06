fa741b95851f5b56de3092d22a58bb0154d1dcba
Matias Torres <matiass.torres@alumnos.frm.utn.edu.ar>
2025-11-06 01:02:19 -0300

    Change port to 8080

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log('💻 Node version:', process.version);
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 let qrCodeImage = null;
 let botStatus = 'Iniciando...';
